809f7b48c2198f1c94ef840c6fa53fe299b5113f
u1_fukui <u1.kobayashi@gmail.com>
2012-07-19 00:39:32 +0900

    発火ソンで作ったu1fukui.comの新デザイン案を追加

---
+++ bootstrap/js/main.js
@@ -0,0 +1,55 @@
+google.load("feeds", "1");
+
+$(function(){
+  $.ajax({
+    type: 'GET',
+    url: 'https://api.github.com/users/u1fukui/repos',
+    cache: false,
+    dataType: 'jsonp',
+    success: function(data) {
+      var datas, i, l, repoElement, reposElement, desc;
+      datas = data.data;
+      reposElement = $('#repositories dl');
+      for(i=0, l=datas.length; i<l; i++) {
+        data = datas[i];
+        repoElement = $('<li><a></a><span class="desc" /></li>');
+        repoElement.find('a').attr('href', data.html_url).html(data.name);
+        desc = (data.description || '').slice(0, 20);
+        if(desc != (data.description || '')) {
+          desc += '...';
+        }
+        repoElement.find('.desc').text(desc);
+
+        if(data.fork){
+          repoElement.addClass('fork');
+        }
+
+        if(data.language){
+          repoElement.addClass('lang-' + data.language.toLowerCase());
+        }
+
+        reposElement.append(repoElement);
+      }
+    }
+  });
+});
+
+function initialize() {
+  var feed = new google.feeds.Feed("http://u1fukui.hateblo.jp/feed");
+  feed.setResultFormat(google.feeds.Feed.JSON_FORMAT);
+  feed.load(function(result) {
+    if (!result.error) {
+      var container = $('#blog_archive ul');
+      for (var i = 0; i < result.feed.entries.length; i++) {
+        var entry = result.feed.entries[i];
+        var li = document.createElement("li");
+        var a = document.createElement("a");
+        a.setAttribute("href", entry.link);
+        a.appendChild(document.createTextNode(entry.title));
+        li.appendChild(a);
+        container.append(li);
+      }
+    }
+  });
+}
+google.setOnLoadCallback(initialize);
